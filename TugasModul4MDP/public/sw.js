924de7f101e4870890a316468e1591ceba6e2a73
Nahari96 <fajarrh@student.ce.undip.ac.id>
2018-11-13 05:23:15 +0700

    TugasmModul4

--- TugasModul4MDP/public/sw.js
+++ TugasModul4MDP/public/sw.js
@@ -9,14 +9,14 @@ self.addEventListener('install', function (event) {
                     '/manifest.json',
                     '/about.html',
                     '/index.html',
-                    'sw.js',
                     '/icon/nahari.jpg',
                     '/icon/nisrina.jpg',
-                    'icon/32.ico',
-                    'icon/96.ico',
-                    'icon/128.ico',
-                    'icon/192.ico',
-                    'icon/256.ico',
+                    '/icon/32.ico',
+                    '/icon/96.ico',
+                    '/icon/128.ico',
+                    '/icon/192.ico',
+                    '/sw.js',
+                    '/icon/256.ico',
                     'https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
                     'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js',
                     'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js',
@@ -25,8 +25,8 @@ self.addEventListener('install', function (event) {
                     'https://unpkg.com/axios/dist/axios.min.js',
                     'https://kodepos-2d475.firebaseio.com/kota_kab/k46.json?print=pretty',
                     'icon/Pulau-Cinta-Gorontalo.jpg',
-                    'https://images.pexels.com/photos/1020315/pexels-photo-1020315.jpeg?auto=compress&cs=tinysrgb&h=350',
                     'https://1.bp.blogspot.com/-ruXt7J3TXBQ/UXJotrg2V5I/AAAAAAAANLw/zG0d2FLanG8/s1600/LOGO+KABUPATEN+POHUWATO.png'
+
                 ]);
             })
     );
